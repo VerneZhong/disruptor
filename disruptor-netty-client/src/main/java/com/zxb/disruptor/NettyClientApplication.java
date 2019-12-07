36718d062cf816a900fae66280967754940b76fb
zhongxuebinwin8 <zhongxuebin171980062@gmail.com>
2019-12-07 18:16:17 +0800

    Disruptor与Netty整合实现百万长链接接入-环境搭建

---
+++ disruptor-netty-client/src/main/java/com/zxb/disruptor/NettyClientApplication.java
@@ -0,0 +1,19 @@
+package com.zxb.disruptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * class
+ *
+ * @author Mr.zxb
+ * @date 2019-12-07 18:01
+ */
+@SpringBootApplication
+public class NettyClientApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(NettyClientApplication.class, args);
+    }
+}
